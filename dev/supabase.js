666108bbcfaaf5882fe90f434f5ada4acb01ff3d
AgileAnarchy <agileanarchygame@gmail.com>
2025-05-12 21:20:47 +0300

    Delete dev/supabase.js

--- dev/supabase.js
+++
@@ -1,6 +0,0 @@
-// supabase.js
-const supabaseUrl = 'https://uyqogqhwxqvjsjgywplp.supabase.co';  // Replace with your Supabase URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5cW9ncWh3eHF2anNqZ3l3cGxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwNjU5NDksImV4cCI6MjA2MjY0MTk0OX0.Dj-aauw8eQbtoNtxkmY9xXmWwJlwjwZZ3eRS28RxHCg';  // Replace with your Supabase public key
-
-// Initialize Supabase client
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
