256953fd49e3c0ceeac04f717b3a51ca55052177
AgileAnarchy <agileanarchygame@gmail.com>
2025-05-12 21:17:26 +0300

    Update dev.js

--- dev/dev.js
+++ dev/dev.js
@@ -3,8 +3,8 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Initialize Supabase
 const supabase = createClient(
-  'https://your-project-id.supabase.co',  // Replace with your Supabase URL
-  'your-anon-public-key'  // Replace with your Supabase public API key
+  'https://uyqogqhwxqvjsjgywplp.supabase.co',  // Replace with your Supabase URL
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5cW9ncWh3eHF2anNqZ3l3cGxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwNjU5NDksImV4cCI6MjA2MjY0MTk0OX0.Dj-aauw8eQbtoNtxkmY9xXmWwJlwjwZZ3eRS28RxHCg'  // Replace with your Supabase public API key
 );
 
 // Handle Create Room Button click
